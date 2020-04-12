9db9aa03b2d640c82cc8dc66d7ed6dcb691bb0ab
Bolarinwa Saheed <refactormyself@users.noreply.github.com>
2020-04-12 09:15:16 +0200

    Add boost dependency

--- Cmake_Boost/main.cpp
+++ Cmake_Boost/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <boost/array.hpp>
+#include <boost/version.hpp> 
+
 #include "head.h"
 using std::cout;
 
 int main() {
  cout << "Hello world, we are powerful." << '\n';
+ boost::array<int, 4> arr = {{1,2,3,4}};
+ cout << "Yes, because we can use Boost Array> Arr[0] = " << arr[0] << '\n';
+
+ cout << "(v1) We are using Boost: "
+          << BOOST_VERSION / 100000     << "."  // major version
+          << BOOST_VERSION / 100 % 1000 << "."  // minor version
+          << BOOST_VERSION % 100                // patch level
+          << std::endl;
+
+ cout << "(v2) Boost version: " << BOOST_LIB_VERSION <<'\n';
 
  return 0;
 }
